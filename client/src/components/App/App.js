0466d55de72c5d05e4b134874da71bbfce3fc5de
Marat <yeffasol@mail.ru>
2020-06-12 11:43:42 +0300

    Добавляет 404 страницу

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import {Switch, Route} from "react-router-dom";
+import Posts from "components/Posts/Posts";
+import NotFound from "components/NotFound/NotFound";
 
 export const App = () =>
     <div className="container">
-
+        <Switch>
+            <Route exact path="/" component={Posts}/>
+            <Route path="/" component={NotFound}/>
+        </Switch>
     </div>;
 
 export default App;
